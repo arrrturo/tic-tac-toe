7c6236c3bef3e07479642bd82d296d6e6313c6c4
Arturo Romero Sánchez <91941978+arrrturo@users.noreply.github.com>
2022-06-02 23:04:16 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿
+namespace _3EnRaya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl3EnRaya = new System.Windows.Forms.Label();
+            this.lblJugadorO = new System.Windows.Forms.Label();
+            this.lblJugadorX = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pbX = new System.Windows.Forms.PictureBox();
+            this.pbO = new System.Windows.Forms.PictureBox();
+            this.pbTablero = new System.Windows.Forms.PictureBox();
+            this.lblPuntosO = new System.Windows.Forms.Label();
+            this.lblPuntosX = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbO)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTablero)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl3EnRaya
+            // 
+            this.lbl3EnRaya.AutoSize = true;
+            this.lbl3EnRaya.Font = new System.Drawing.Font("Unispace", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3EnRaya.Location = new System.Drawing.Point(13, 17);
+            this.lbl3EnRaya.Name = "lbl3EnRaya";
+            this.lbl3EnRaya.Size = new System.Drawing.Size(148, 29);
+            this.lbl3EnRaya.TabIndex = 11;
+            this.lbl3EnRaya.Text = "3 EN RAYA";
+            // 
+            // lblJugadorO
+            // 
+            this.lblJugadorO.AutoSize = true;
+            this.lblJugadorO.Font = new System.Drawing.Font("Unispace", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJugadorO.Location = new System.Drawing.Point(34, 148);
+            this.lblJugadorO.Name = "lblJugadorO";
+            this.lblJugadorO.Size = new System.Drawing.Size(79, 19);
+            this.lblJugadorO.TabIndex = 12;
+            this.lblJugadorO.Text = "JUGADOR";
+            // 
+            // lblJugadorX
+            // 
+            this.lblJugadorX.AutoSize = true;
+            this.lblJugadorX.Font = new System.Drawing.Font("Unispace", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJugadorX.Location = new System.Drawing.Point(34, 241);
+            this.lblJugadorX.Name = "lblJugadorX";
+            this.lblJugadorX.Size = new System.Drawing.Size(79, 19);
+            this.lblJugadorX.TabIndex = 13;
+            this.lblJugadorX.Text = "JUGADOR";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Font = new System.Drawing.Font("Unispace", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevo.Location = new System.Drawing.Point(38, 75);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 16;
+            this.btnNuevo.Text = "NUEVA";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Unispace", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(38, 341);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 18;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // pb2
+            // 
+            this.pb2.BackgroundImage = global::_3EnRaya.Properties.Resources.x;
+            this.pb2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pb2.Location = new System.Drawing.Point(332, 28);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(92, 97);
+            this.pb2.TabIndex = 27;
+            this.pb2.TabStop = false;
+            this.pb2.Click += new System.EventHandler(this.pb2_Click);
+            // 
+            // pb6
+            // 
+            this.pb6.BackgroundImage = global::_3EnRaya.Properties.Resources.x;
+            this.pb6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pb6.Location = new System.Drawing.Point(453, 148);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(92, 97);
+            this.pb6.TabIndex = 26;
+            this.pb6.TabStop = false;
+            this.pb6.Click += new System.EventHandler(this.pb6_Click);
+            // 
+            // pb8
+            // 
+            this.pb8.BackgroundImage = global::_3EnRaya.Properties.Resources.x;
+            this.pb8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pb8.Location = new System.Drawing.Point(331, 267);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(92, 97);
+            this.pb8.TabIndex = 25;
+            this.pb8.TabStop = false;
+            this.pb8.Click += new System.EventHandler(this.pb8_Click);
+            // 
+            // pb4
+            // 
+            this.pb4.BackgroundImage = global::_3EnRaya.Properties.Resources.x;
+            this.pb4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pb4.Location = new System.Drawing.Point(212, 148);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(92, 97);
+            this.pb4.TabIndex = 24;
+            this.pb4.TabStop = false;
+            this.pb4.Click += new System.EventHandler(this.pb4_Click);
+            // 
+            // pb9
+            // 
+            this.pb9.BackgroundImage = global::_3EnRaya.Properties.Resources.circulo;
+            this.pb9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pb9.Location = new System.Drawing.Point(453, 267);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(93, 97);
+            this.pb9.TabIndex = 23;
+            this.pb9.TabStop = false;
+            this.pb9.Click += new System.EventHandler(this.pb9_Click);
+            // 
+            // pb7
+            // 
+            this.pb7.BackgroundImage = global::_3EnRaya.Properties.Resources.circulo;
+            this.pb7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pb7.Location = new System.Drawing.Point(210, 267);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(93, 97);
+            this.pb7.TabIndex = 22;
+            this.pb7.TabStop = false;
+            this.pb7.Click += new System.EventHandler(this.pb7_Click);
+            // 
+            // pb3
+            // 
+            this.pb3.BackgroundImage = global::_3EnRaya.Properties.Resources.circulo;
+            this.pb3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pb3.Location = new System.Drawing.Point(453, 28);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(93, 97);
+            this.pb3.TabIndex = 21;
+            this.pb3.TabStop = false;
+            this.pb3.Click += new System.EventHandler(this.pb3_Click);
+            // 
+            // pb5
+            // 
+            this.pb5.BackgroundImage = global::_3EnRaya.Properties.Resources.circulo;
+            this.pb5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pb5.Location = new System.Drawing.Point(331, 148);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(93, 97);
+            this.pb5.TabIndex = 20;
+            this.pb5.TabStop = false;
+            this.pb5.Click += new System.EventHandler(this.pb5_Click);
+            // 
+            // pb1
+            // 
+            this.pb1.BackgroundImage = global::_3EnRaya.Properties.Resources.circulo;
+            this.pb1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pb1.Location = new System.Drawing.Point(211, 28);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(93, 97);
+            this.pb1.TabIndex = 19;
+            this.pb1.TabStop = false;
+            this.pb1.Click += new System.EventHandler(this.pb1_Click);
+            // 
+            // pbX
+            // 
+            this.pbX.BackgroundImage = global::_3EnRaya.Properties.Resources.x;
+            this.pbX.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbX.Location = new System.Drawing.Point(119, 227);
+            this.pbX.Name = "pbX";
+            this.pbX.Size = new System.Drawing.Size(42, 42);
+            this.pbX.TabIndex = 15;
+            this.pbX.TabStop = false;
+            // 
+            // pbO
+            // 
+            this.pbO.BackgroundImage = global::_3EnRaya.Properties.Resources.circulo;
+            this.pbO.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbO.Location = new System.Drawing.Point(119, 133);
+            this.pbO.Name = "pbO";
+            this.pbO.Size = new System.Drawing.Size(42, 42);
+            this.pbO.TabIndex = 14;
+            this.pbO.TabStop = false;
+            // 
+            // pbTablero
+            // 
+            this.pbTablero.BackgroundImage = global::_3EnRaya.Properties.Resources._7fd654e4ed2675f4606bd72177eb1fb2;
+            this.pbTablero.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbTablero.Location = new System.Drawing.Point(198, 17);
+            this.pbTablero.Name = "pbTablero";
+            this.pbTablero.Size = new System.Drawing.Size(361, 360);
+            this.pbTablero.TabIndex = 0;
+            this.pbTablero.TabStop = false;
+            // 
+            // lblPuntosO
+            // 
+            this.lblPuntosO.AutoSize = true;
+            this.lblPuntosO.Font = new System.Drawing.Font("Unispace", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuntosO.Location = new System.Drawing.Point(38, 178);
+            this.lblPuntosO.Name = "lblPuntosO";
+            this.lblPuntosO.Size = new System.Drawing.Size(79, 19);
+            this.lblPuntosO.TabIndex = 28;
+            this.lblPuntosO.Text = "PuntosO";
+            // 
+            // lblPuntosX
+            // 
+            this.lblPuntosX.AutoSize = true;
+            this.lblPuntosX.Font = new System.Drawing.Font("Unispace", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuntosX.Location = new System.Drawing.Point(38, 272);
+            this.lblPuntosX.Name = "lblPuntosX";
+            this.lblPuntosX.Size = new System.Drawing.Size(79, 19);
+            this.lblPuntosX.TabIndex = 29;
+            this.lblPuntosX.Text = "PuntosX";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 403);
+            this.Controls.Add(this.lblPuntosX);
+            this.Controls.Add(this.lblPuntosO);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb6);
+            this.Controls.Add(this.pb8);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb9);
+            this.Controls.Add(this.pb7);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.pbX);
+            this.Controls.Add(this.pbO);
+            this.Controls.Add(this.lblJugadorX);
+            this.Controls.Add(this.lblJugadorO);
+            this.Controls.Add(this.lbl3EnRaya);
+            this.Controls.Add(this.pbTablero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbO)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTablero)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbTablero;
+        private System.Windows.Forms.Label lbl3EnRaya;
+        private System.Windows.Forms.Label lblJugadorO;
+        private System.Windows.Forms.Label lblJugadorX;
+        private System.Windows.Forms.PictureBox pbO;
+        private System.Windows.Forms.PictureBox pbX;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb2;
+        private System.Windows.Forms.Label lblPuntosO;
+        private System.Windows.Forms.Label lblPuntosX;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3EnRaya
+{
+    public partial class Form1 : Form
+    {
+        LogicaInterfaz log = new LogicaInterfaz();
+        List<Jugador> listaGuadores;
+        Jugador jug0;
+        Jugador jug1;
+        PictureBox[] posTablero;
+
+        int equisOCirculo = 0;
+
+        public Form1()
+        {
+            posTablero = new PictureBox[9];
+            
+            InitializeComponent();
+
+            posTablero[0] = pb1;
+            posTablero[1] = pb2;
+            posTablero[2] = pb3;
+            posTablero[3] = pb4;
+            posTablero[4] = pb5;
+            posTablero[5] = pb6;
+            posTablero[6] = pb7;
+            posTablero[7] = pb8;
+            posTablero[8] = pb9;
+           
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listaGuadores = new List<Jugador>();
+            jug0 = new Jugador("Jugador O", 0);
+            jug1 = new Jugador("Jugador X", 1);
+
+            listaGuadores.Add(jug0);
+            listaGuadores.Add(jug1);
+
+            pararTablero();
+
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            foreach (var pic in posTablero)
+            {
+                pic.Enabled = true;
+                pic.BackgroundImage = null;
+                pic.Tag = "";
+            }
+
+            lblPuntosO.Text = jug0.Puntos.ToString();
+            lblPuntosX.Text = jug1.Puntos.ToString();
+        }
+
+
+       
+
+        private void pb1_Click(object sender, EventArgs e)
+        {
+            clickPos(pb1);
+        }
+
+        private void pb2_Click(object sender, EventArgs e)
+        {
+            clickPos(pb2);
+        }
+
+        private void pb3_Click(object sender, EventArgs e)
+        {
+            clickPos(pb3);
+        }
+
+        private void pb4_Click(object sender, EventArgs e)
+        {
+            clickPos(pb4);
+        }
+
+        private void pb5_Click(object sender, EventArgs e)
+        {
+            clickPos(pb5);
+        }
+
+        private void pb6_Click(object sender, EventArgs e)
+        {
+            clickPos(pb6);
+        }
+
+        private void pb7_Click(object sender, EventArgs e)
+        {
+            clickPos(pb7);
+        }
+
+        private void pb8_Click(object sender, EventArgs e)
+        {
+            clickPos(pb8);
+        }
+
+        private void pb9_Click(object sender, EventArgs e)
+        {
+            clickPos(pb9);
+        }
+
+
+        private void pararTablero()
+        {
+            foreach (var pic in posTablero)
+            {
+                pic.Enabled = false;
+            }
+        }
+        private void quienGana()
+        {
+            if (pb1.Tag.ToString() == "1" && pb2.Tag.ToString() == "1" && pb3.Tag.ToString() == "1")
+            {
+                jug1.Puntos++;
+                lblPuntosX.Text = listaGuadores[1].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb1.Tag.ToString() == "0" && pb2.Tag.ToString() == "0" && pb3.Tag.ToString() == "0")
+            {
+                jug0.Puntos++;
+                lblPuntosO.Text = listaGuadores[0].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb4.Tag.ToString() == "1" && pb5.Tag.ToString() == "1" && pb6.Tag.ToString() == "1")
+            {
+                jug1.Puntos++;
+                lblPuntosX.Text = listaGuadores[1].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb4.Tag.ToString() == "0" && pb5.Tag.ToString() == "0" && pb6.Tag.ToString() == "0")
+            {
+                jug0.Puntos++;
+                lblPuntosO.Text = listaGuadores[0].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb7.Tag.ToString() == "1" && pb8.Tag.ToString() == "1" && pb9.Tag.ToString() == "1")
+            {
+                jug1.Puntos++;
+                lblPuntosX.Text = listaGuadores[1].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb7.Tag.ToString() == "0" && pb8.Tag.ToString() == "0" && pb9.Tag.ToString() == "0")
+            {
+                jug0.Puntos++;
+                lblPuntosO.Text = listaGuadores[0].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb1.Tag.ToString() == "1" && pb4.Tag.ToString() == "1" && pb7.Tag.ToString() == "1")
+            {
+                jug1.Puntos++;
+                lblPuntosX.Text = listaGuadores[1].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb1.Tag.ToString() == "0" && pb4.Tag.ToString() == "0" && pb7.Tag.ToString() == "0")
+            {
+                jug0.Puntos++;
+                lblPuntosO.Text = listaGuadores[0].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb2.Tag.ToString() == "1" && pb5.Tag.ToString() == "1" && pb8.Tag.ToString() == "1")
+            {
+                jug1.Puntos++;
+                lblPuntosX.Text = listaGuadores[1].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb2.Tag.ToString() == "0" && pb5.Tag.ToString() == "0" && pb8.Tag.ToString() == "0")
+            {
+                jug0.Puntos++;
+                lblPuntosO.Text = listaGuadores[0].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb3.Tag.ToString() == "1" && pb6.Tag.ToString() == "1" && pb9.Tag.ToString() == "1")
+            {
+                jug1.Puntos++;
+                lblPuntosX.Text = listaGuadores[1].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb3.Tag.ToString() == "0" && pb6.Tag.ToString() == "0" && pb9.Tag.ToString() == "0")
+            {
+                jug0.Puntos++;
+                lblPuntosO.Text = listaGuadores[0].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb1.Tag.ToString() == "1" && pb5.Tag.ToString() == "1" && pb9.Tag.ToString() == "1")
+            {
+                jug1.Puntos++;
+                lblPuntosX.Text = listaGuadores[1].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb1.Tag.ToString() == "0" && pb5.Tag.ToString() == "0" && pb9.Tag.ToString() == "0")
+            {
+                jug0.Puntos++;
+                lblPuntosO.Text = listaGuadores[0].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb3.Tag.ToString() == "1" && pb5.Tag.ToString() == "1" && pb7.Tag.ToString() == "1")
+            {
+                jug1.Puntos++;
+                lblPuntosX.Text = listaGuadores[1].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+            if (pb3.Tag.ToString() == "0" && pb5.Tag.ToString() == "0" && pb7.Tag.ToString() == "0")
+            {
+                jug0.Puntos++;
+                lblPuntosO.Text = listaGuadores[0].Puntos.ToString();
+
+                pararTablero();
+
+                return;
+            }
+
+
+
+        }
+
+        private void clickPos(PictureBox p)
+        {
+            log.EfectoPulsacion(p);
+  
+            if (listaGuadores[0].Numero == equisOCirculo)
+            {
+                p.BackgroundImage = Properties.Resources.circulo;
+                p.Tag = "0";
+            }
+
+            if (listaGuadores[1].Numero == equisOCirculo)
+            {
+                p.BackgroundImage = Properties.Resources.x;
+                p.Tag = "1";
+            }
+            p.Enabled = false;
+
+            quienGana();
+            equisOcirculo();
+        }
+
+        private void equisOcirculo()
+        {
+
+            equisOCirculo++;
+
+            if (equisOCirculo % 2 == 0)
+            {
+                equisOCirculo = 0;
+            }
+            if (equisOCirculo % 2 == 1)
+            {
+                equisOCirculo = 1;
+            }
+           
+        }
+    }
+}
